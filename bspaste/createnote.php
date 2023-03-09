926e1ff3cd4d3fdc768455709b08d63a79c6fbd9
Syahda Fahreza <syahdafahreza@gmail.com>
2023-03-09 18:31:26 +0700

    Add encryption feature

--- bspaste/createnote.php
+++ bspaste/createnote.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $databaseHost = 'localhost';
 $databaseName = 'np';
 $databaseUsername = 'root';
@@ -6,24 +6,43 @@ $databasePassword = '';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
-if($_GET['act']== 'tambahnotes'){
-    $namauser = '\''. $_POST['nama_user']."'";
-    $judulbaru = '\''. $_POST['judul_note_baru']."'";
-    $isinotebaru = '\''. $_POST['isi_note_baru']."'";
+if ($_GET['act'] == 'tambahnotes') {
+    $namauser = '\'' . $_POST['nama_user'] . "'";
+    // $judulbaru = "\"" . $_POST['judul_note_baru'] . "\"";
+    // $isinotebaru = "\"" . $_POST['isi_note_baru'] . "\"";
+    $judulbaru = $_POST['judul_note_baru'];
+    $isinotebaru = $_POST['isi_note_baru'];
     // $tanggalsekarang = '\''. mysqli_query($mysqli, "SELECT NOW()")."'";
 
-//query buat
-$querytambah = mysqli_query($mysqli,"INSERT INTO `notes` (id, user, title, text) VALUES (NULL, $namauser, $judulbaru, $isinotebaru)");
+    // Encryption Judul
+    $key = 'pass';
+    $plaintext_judulbaru = $judulbaru;
+    $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+    $iv = openssl_random_pseudo_bytes($ivlen);
+    $ciphertext_raw = openssl_encrypt($plaintext_judulbaru, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+    $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+    $ciphertext_judulbaru = base64_encode($iv . $hmac . $ciphertext_raw);
 
-if ($querytambah) {
- # credirect ke page index
- header("location: notes.php");
-}
-else{
- echo "ERROR, data gagal diupdate". mysqli_error($mysqli);
-}
+    // Encryption Isi Teks
+    $key = 'pass';
+    $plaintext_isinotebaru = $isinotebaru;
+    $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+    $iv = openssl_random_pseudo_bytes($ivlen);
+    $ciphertext_raw = openssl_encrypt($plaintext_isinotebaru, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+    $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+    $ciphertext_isinotebaru = base64_encode($iv . $hmac . $ciphertext_raw);
 
-//mysql_close($host);
+    //query buat
+    $querytambah = mysqli_query($mysqli, "INSERT INTO `notes` (id, user, title, text) VALUES (NULL, $namauser, '$ciphertext_judulbaru', '$ciphertext_isinotebaru')");
+
+    if ($querytambah) {
+        # credirect ke page index
+        header("location: notes.php");
+    } else {
+        echo "ERROR, data gagal diupdate" . mysqli_error($mysqli);
+    }
+
+    //mysql_close($host);
 }
 
 ?>
--- bspaste/editnote.php
+++ bspaste/editnote.php
@@ -10,8 +10,26 @@ $id = $_GET['id_note'];
 $judul = $_GET['judul_note'];
 $isinote = $_GET['isi_note'];
 
+// Encryption Judul
+$key = 'pass';
+$plaintext_judulbaru = $judul;
+$ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+$iv = openssl_random_pseudo_bytes($ivlen);
+$ciphertext_raw = openssl_encrypt($plaintext_judulbaru, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+$ciphertext_juduledit = base64_encode($iv . $hmac . $ciphertext_raw);
+
+// Encryption Isi Teks
+$key = 'pass';
+$plaintext_isinotebaru = $isinote;
+$ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
+$iv = openssl_random_pseudo_bytes($ivlen);
+$ciphertext_raw = openssl_encrypt($plaintext_isinotebaru, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+$ciphertext_isinoteedit = base64_encode($iv . $hmac . $ciphertext_raw);
+
 //query update
-$query = mysqli_query($mysqli,"UPDATE `notes` SET title='$judul' , text='$isinote' WHERE id='$id' ");
+$query = mysqli_query($mysqli,"UPDATE `notes` SET title='$ciphertext_juduledit' , text='$ciphertext_isinoteedit' WHERE id='$id' ");
 
 if ($query) {
  # credirect ke page index
