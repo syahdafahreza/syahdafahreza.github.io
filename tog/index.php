860188b096be0c976dcf80d2bd7f8c05caeabad8
Syahda Fahreza <syahdafahreza@gmail.com>
2023-03-13 16:11:39 +0700

    .

--- tog/index.php
+++ tog/index.php
@@ -53,9 +53,10 @@ if (!$adalahadmin) {
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+                <div class="sidebar-brand-icon">
+                    <!-- <i class="fas fa-laugh-wink"></i> -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 100 300" id="mainlogo"><defs></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M91.66,138.71,41.31,116.25C19.73,106.66,5.36,93.86,1.25,78.51A34.31,34.31,0,0,0,0,87.17c.27,19.24,15.78,35.08,41.31,46.42l50.35,22.46c21.58,9.57,35.94,22.39,40.06,37.73a34.76,34.76,0,0,0,1.24-8.66C132.7,165.89,117.19,150.05,91.66,138.71Z"/><path class="cls-1" d="M42.51,40.16c-19.4,9.82-27,21.17-26.82,29.67a18.1,18.1,0,0,0,2.23,8.65c3.92-7.61,13.2-16.1,29.75-23.45L132.9,17,133,0S66.77,29.41,42.51,40.16Z"/><path class="cls-1" d="M83.45,90.52c-10.08,5.1-13.41,10.59-13.2,13.6-.15,2,1.32,5.23,5.44,8.65a44.87,44.87,0,0,1,10.67-6.32L133,85.66v-17Z"/><path class="cls-1" d="M90.46,232.14c19.4-9.82,27-21.18,26.82-29.68a18.28,18.28,0,0,0-2.23-8.66c-3.92,7.62-13.21,16.11-29.76,23.47-2.58,1.13-85.22,38-85.22,38l-.07,17S66.21,242.87,90.46,232.14Z"/><path class="cls-1" d="M49.52,181.77c10.08-5.09,13.42-10.59,13.2-13.59.14-2-1.32-5.24-5.44-8.66a44.73,44.73,0,0,1-10.67,6.33L0,186.62v17Z"/></g></g></svg>
                 </div>
                 <div class="sidebar-brand-text mx-3">Token of Gratitude</div>
             </a>
@@ -65,7 +66,7 @@ if (!$adalahadmin) {
 
             <!-- Nav Item -->
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -149,32 +150,34 @@ if (!$adalahadmin) {
                     <!-- </div> -->
                     <!-- </form> -->
 
+                    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
+                        <!-- <li class="nav-item dropdown no-arrow d-sm-none"> -->
+                            <!-- <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" -->
+                                <!-- data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> -->
+                                <!-- <i class="fas fa-search fa-fw"></i> -->
+                            <!-- </a> -->
                             <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
+                            <!-- <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" -->
+                                <!-- aria-labelledby="searchDropdown"> -->
+                                <!-- <form class="form-inline mr-auto w-100 navbar-search"> -->
+                                    <!-- <div class="input-group"> -->
+                                        <!-- <input type="text" class="form-control bg-light border-0 small" -->
+                                            <!-- placeholder="Search for..." aria-label="Search" -->
+                                            <!-- aria-describedby="basic-addon2"> -->
+                                        <!-- <div class="input-group-append"> -->
+                                            <!-- <button class="btn btn-primary" type="button"> -->
+                                                <!-- <i class="fas fa-search fa-sm"></i> -->
+                                            <!-- </button> -->
+                                        <!-- </div> -->
+                                    <!-- </div> -->
+                                <!-- </form> -->
+                            <!-- </div> -->
+                        <!-- </li> -->
 
                         <!-- Nav Item - Alerts -->
                         <li class="nav-item dropdown no-arrow mx-1">
@@ -360,7 +363,7 @@ if (!$adalahadmin) {
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Selama Datang!</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Selamat Datang!</h1>
                         <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i -->
                         <!-- class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
                     </div>
@@ -372,14 +375,14 @@ if (!$adalahadmin) {
                             <!-- Illustrations -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Bootstrap Paste</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Token of Gratitude (ToG)</h6>
                                 </div>
                                 <div class="card-body">
                                     <div class="text-center">
                                         <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
                                             src="img/undraw_posting_photo.svg" alt="...">
                                     </div>
-                                    <p>Apa itu Bootstrap Paste? Bootstrap Paste adalah situs salin dan tempel catatan
+                                    <p>Apa itu ToG? ToG adalah situs salin dan tempel catatan
                                         sederhana. Ini dirancang untuk membantu Anda melacak pemikiran dan ide Anda
                                         dengan menawarkan cara mudah untuk menyimpan dan mengakses catatan Anda secara
                                         online.</p>
