b957ccf700256db21163beecb1b1c243e0ad19cf
Syahda Fahreza <syahdafahreza@gmail.com>
2023-03-25 11:48:34 +0700

    .

--- testlab/bukukita/app/config/config.php
+++ testlab/bukukita/app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('base_url', 'http://localhost/testlab/bukita/public');
+define('base_url', '/testlab/bukukita/public');
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
