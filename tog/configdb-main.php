bb2c2b7446270e9c413e47ed15000210b0d7f2ed
Syahda Fahreza <syahdafahreza@gmail.com>
2023-04-01 04:46:29 +0700

    .

--- tog/configdb-main.php
+++ tog/configdb-main.php
@@ -1,8 +1,8 @@
 <?php
-$databaseHost = 'localhost';
-$databaseName = 'tog';
-$databaseUsername = 'root';
-$databasePassword = '';
+$databaseHost = 'sql306.epizy.com';
+$databaseUsername = 'epiz_33317858';
+$databasePassword = 'aRodYdr6aMF7L';
+$databaseName = 'epiz_33317858_tog';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 ?>
