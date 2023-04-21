88cbd11d54ee5da225aa0d3bf9dbccea8e4cbca2
Syahda Fahreza <syahdafahreza@gmail.com>
2023-04-21 23:13:29 +0700

    .

--- wallpaper/bluearchive/recollectionlobby/sunaookamishiroko_v2/assets/index.e2ce666a.js
+++ wallpaper/bluearchive/recollectionlobby/sunaookamishiroko_v2/assets/index.e2ce666a.js
@@ -61503,7 +61503,7 @@ PERFORMANCE OF THIS SOFTWARE.
                   event: function (K, et) {
                     if (et.data.name.indexOf("MemorialLobby") >= 0) {
                       let it = et.data.name.split("/");
-                      (d.src = `./sound/${it[it.length - 1]}.wav`),
+                      (d.src = `./Sound/${it[it.length - 1]}.wav`),
                         d.play(),
                         e.language != "" &&
                           ((a.innerHTML = s[e.language][et.data.name]),
@@ -61548,7 +61548,7 @@ PERFORMANCE OF THIS SOFTWARE.
                 });
             }),
             L(),
-            (p.src = "./sound/Theme.wav"),
+            (p.src = "./Sound/Theme.wav"),
             (p.loop = !0),
             (p.volume = e.bgmVolume),
             p.play(),
