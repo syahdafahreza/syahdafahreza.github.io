d64bac80c03cfa73117c9c46769b12db951f762c
Syahda Fahreza <syahdafahreza@gmail.com>
2023-05-05 08:38:35 +0700

    Moved testlab to another repository

--- testlab/bukukita/app/controllers/Logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Logout {
-	public function Logout(){
-		session_start();
-		session_destroy();
-		header('location: '. base_url . '/login');
-	}
-}
--- testlab/musicplayerci/application/views/musice_explorer.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: WebKit
- * Date: 11/21/2016
- * Time: 5:42 PM
- */
